bda6bf05a762bd48d8af1ad2eec5cc48011fbc76
Akshay7016 <asajgule@gmail.com>
2024-12-24 12:49:25 +0530

    authentication implemented

---
+++ controllers/auth.controller.js
@@ -0,0 +1,76 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+import { prisma } from "../lib/prisma.js";
+
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return res.status(201).json({
+      message: "User created successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Failed to create user!" });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid Credentials!" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid Credentials!" });
+    }
+
+    const age = 1000 * 60 * 60 * 24 * 7; // 7 days
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+      },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: age,
+      }
+    );
+
+    delete user.password;
+
+    // TODO: add secure:true in 3rd arg object in production mode
+    return res
+      .cookie("token", token, { httpOnly: true, maxAge: age })
+      .status(200)
+      .json(user);
+  } catch (error) {
+    console.log(error.data);
+    return res.status(500).json({ message: "Failed to login!" });
+  }
+};
+
+export const logout = (req, res) => {
+  return res
+    .clearCookie("token")
+    .status(200)
+    .json({ message: "Logout successful" });
+};
--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import authRoute from "./routes/auth.route.js";
 
 const app = express();
+
+// To allow JSON objects from client
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
   console.log("Server is running!");
