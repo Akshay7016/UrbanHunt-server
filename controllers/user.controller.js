4f1fa5a7763c2059c575f3a5e2ffd04c42c372f4
Akshay7016 <asajgule@gmail.com>
2025-01-01 21:10:28 +0530

    Message notification count handled

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -138,3 +138,27 @@ export const getProfilePosts = async (req, res) => {
     return res.status(500).json({ message: "Failed to get profile posts!" });
   }
 };
+
+export const getNotificationNumber = async (req, res) => {
+  const tokenUserId = req.userId;
+
+  try {
+    const count = await prisma.chat.count({
+      where: {
+        userIDs: {
+          hasSome: [tokenUserId],
+        },
+        NOT: {
+          seenBy: {
+            hasSome: [tokenUserId],
+          },
+        },
+      },
+    });
+    return res.status(200).json(count);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Failed to get notifications count!" });
+  }
+};
--- routes/user.route.js
+++ routes/user.route.js
@@ -7,6 +7,7 @@ import {
   deleteUser,
   savePost,
   getProfilePosts,
+  getNotificationNumber,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
@@ -18,5 +19,6 @@ router.put("/:id", verifyToken, updateUser);
 router.delete("/:id", verifyToken, deleteUser);
 router.post("/save", verifyToken, savePost);
 router.get("/profilePosts", verifyToken, getProfilePosts);
+router.get("/notification", verifyToken, getNotificationNumber);
 
 export default router;
